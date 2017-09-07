37080f431f11ba3c6cf8ff685ee8b3646ca33a66
Lucas Felipe <30655133+luksave@users.noreply.github.com>
2017-09-07 20:55:36 -0300

    Primeiro commit

---
+++ src/main.java
@@ -0,0 +1,16 @@
+//Objetivos:
+/*
+ * 1 - Criar a tabela de transição do DFA
+ * 2 - Criar a tabela de dados
+ * 		2.1 - Preencher a tabela de dados com as palavras reservadas da linguagem
+ * 3 - Criar Método(s) que reconhece(rão) Tokens
+ * 4 - Criar rotina de reconhecimento de Tokens 
+ * */
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
